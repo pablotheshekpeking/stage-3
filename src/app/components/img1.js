e55876b1114e4d3085e21ac1f0eb961fa7c65603
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 02:36:01 +0300

    lets see

--- src/app/components/img1.js
+++ src/app/components/img1.js
@@ -5,9 +5,9 @@ import React from 'react';
 
 const No1 = () => {
     return ( 
-        <Stack w={'100%'}>
+        <Stack w={'100%'} p={'20px'}>
             <Box w={'50%'}></Box>
-            <Box w={'50%'}>
+            <Box w={'50%'} float={'right'}>
                 <Img src='/assets/img1.png' w={'100%'} h={'auto'} />
             </Box>
         </Stack>
