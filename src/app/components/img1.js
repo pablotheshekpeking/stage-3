023ea9aef4a25bb86b70c188c847440a92f11762
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 02:28:55 +0300

    lets seen again

---
+++ src/app/components/img1.js
@@ -0,0 +1,17 @@
+'use client'
+
+import { Stack, Box, Img } from '@chakra-ui/react';
+import React from 'react';
+
+const No1 = () => {
+    return ( 
+        <Stack w={'100%'}>
+            <Box w={'50%'}></Box>
+            <Box w={'50%'}>
+                <Img src='/assets/img1.png' w={'100%'} h={'auto'} />
+            </Box>
+        </Stack>
+     );
+}
+ 
+export default No1;
