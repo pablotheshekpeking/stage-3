25fcc89abd276d6423af7638a26c3c59f116a065
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 01:31:44 +0300

    go again

--- src/app/signin/page.js
+++ src/app/signin/page.js
@@ -2,6 +2,7 @@
 
 import { Stack, Box, Text, Input, InputGroup, InputRightElement, Button } from "@chakra-ui/react";
 import Form from "../components/form";
+import Link from "next/link";
 import React from 'react';
 
 
@@ -9,7 +10,7 @@ const Signin = () => {
     return (
         <>
             <Stack bg={'blue.400'} p={'20px'} w={'100%'}>
-                <Text textAlign={'center'} color={'white'}>Drag & Drop Images</Text>
+                <Link href={'../'}><Text textAlign={'center'} color={'white'}>Drag & Drop Images</Text></Link>
             </Stack>
 
             <Stack direction={'row'} className="body" w={'100%'}>
