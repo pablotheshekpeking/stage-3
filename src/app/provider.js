63cb35218758aa5d2421b1c35a3fc491b8b8fea1
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 02:31:45 +0300

    tobi

--- src/app/provider.js
+++ src/app/provider.js
@@ -3,12 +3,14 @@
 import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import Navbar from './components/navbar';
+import No1 from './components/img1';
 
 export function Providers({ children }) {
   return (
     <CacheProvider>
       <ChakraProvider>
         <Navbar />
+        <No1 />
       </ChakraProvider>
     </CacheProvider>
   );
