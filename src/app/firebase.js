978c60739302b3277478d7d0db025e21e3d2d0ae
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 17:29:37 +0300

    please abeg

--- src/app/firebase.js
+++ src/app/firebase.js
@@ -16,6 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
