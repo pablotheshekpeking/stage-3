5ad8975f39ef398356b2c3ed644b9f0d329abe0a
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-19 13:25:27 +0300

    again

--- src/app/gallery/provider.js
+++ src/app/gallery/provider.js
@@ -6,6 +6,8 @@ import { Stack, Box } from '@chakra-ui/react';
 import Navbar from '../components/navbar';
 import No1 from '../components/img1';
 import No2 from '../components/img2';
+import No3 from '../components/img3';
+import No4 from '../components/img4';
 
 export function Providers({ children }) {
   return (
@@ -13,8 +15,10 @@ export function Providers({ children }) {
       <ChakraProvider>
         <Navbar />
         <Stack direction={'row'}>
-          <Box><No1 /></Box>
-          <Box><No2 />  </Box>
+          <Box w={'25%'}><No1 /></Box>
+          <Box w={'25%'}><No2 /></Box>
+          <Box w={'25%'}><No3 /></Box>
+          <Box w={'25%'}><No4 /></Box>
         </Stack>
       </ChakraProvider>
     </CacheProvider>
