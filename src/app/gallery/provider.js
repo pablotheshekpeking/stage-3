1db9a8cadb2e0344085bf49eee0ad2de87d3e7ba
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-20 14:43:19 +0300

    again

--- src/app/gallery/provider.js
+++ src/app/gallery/provider.js
@@ -4,10 +4,6 @@ import { CacheProvider } from '@chakra-ui/next-js';
 import { ChakraProvider } from '@chakra-ui/react';
 import { Stack, Box } from '@chakra-ui/react';
 import Navbar from '../components/navbar';
-import No1 from '../components/img1';
-import No2 from '../components/img2';
-import No3 from '../components/img3';
-import No4 from '../components/img4';
 import SearchBar from '../components/searchbar';
 import MovieGrid from '../components/MovieGrid';
 
