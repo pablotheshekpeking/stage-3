db8a1b3077c14343751958e4cf8b8614ab1a8152
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-20 14:53:55 +0300

    again?

--- src/app/components/MovieGrid.js
+++ src/app/components/MovieGrid.js
@@ -1,42 +1,80 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import Card from "./card";
 import tmdbApi from "../api"; // Import the tmdbApi instance
 
 const MovieGrid = () => {
   const [cardsData, setCardsData] = useState([]);
+  const [page, setPage] = useState(1); // Track the current page
+  const [loading, setLoading] = useState(false); // Track loading state
+  const containerRef = useRef(null);
+
+  const fetchMovies = async () => {
+    try {
+      setLoading(true);
+
+      const response = await tmdbApi.get("/movie/top_rated", {
+        params: {
+          page,
+          language: "en-US",
+        },
+      });
+
+      const newMovies = response.data.results;
+
+      // Append new movies to the existing data
+      setCardsData((prevData) => [...prevData, ...newMovies]);
+
+      setPage(page + 1);
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  // Load more movies when scrolling to the bottom of the container
+  const handleScroll = () => {
+    const container = containerRef.current;
+    if (container) {
+      if (
+        container.scrollTop + container.clientHeight >=
+        container.scrollHeight - 20
+      ) {
+        fetchMovies();
+      }
+    }
+  };
 
   useEffect(() => {
-    const fetchTopRatedMovies = async () => {
-      try {
-        const response = await tmdbApi.get("/movie/top_rated", {
-          params: {
-            page: 1, // Page 1
-            language: "en-US", // Language
-          },
-        });
+    // Initial fetch
+    fetchMovies();
 
-        const top10Movies = response.data.results.slice(0, 10);
+    // Attach scroll event listener
+    const container = containerRef.current;
+    container.addEventListener("scroll", handleScroll);
 
-        setCardsData(top10Movies);
-      } catch (error) {
-        console.error("Error fetching top-rated movies:", error);
-      }
+    return () => {
+      // Remove the scroll event listener when unmounting
+      container.removeEventListener("scroll", handleScroll);
     };
-
-    fetchTopRatedMovies();
   }, []);
 
   return (
     <div
+      ref={containerRef}
       style={{
         display: "grid",
         gridTemplateColumns: "repeat(2, 1fr)",
         gap: "20px",
+        minHeight: "100vh", // Ensure container takes up at least the full viewport height
+        overflowY: "auto", // Enable vertical scrolling
       }}
     >
       {cardsData.map((movieData) => (
         <Card key={movieData.id} movie={movieData} />
       ))}
+
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
