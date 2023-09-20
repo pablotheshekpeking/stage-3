ced924651e57cfa7f852273317ce0b9e2f36d492
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-20 14:56:39 +0300

    lets see

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -7,11 +7,9 @@ const Card = ({ movie }) => {
   const imageUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
 
   return (
-    <Stack direction={'column'} data-testid="movie-card">
       <Box mb={'20px'} w={'full'} h={['200px', '200px', '400px', '400px']}>
         <Img src={imageUrl} alt={movie.title} />
       </Box>
-    </Stack>
   );
 }
 
