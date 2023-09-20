2b4d3c249442fcc64c388799df64130f20c299ac
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-20 14:46:58 +0300

    maybe?

--- src/app/components/card.js
+++ src/app/components/card.js
@@ -4,19 +4,16 @@ import { Text, Heading, Stack, Box, Img, Show } from "@chakra-ui/react";
 import React, { useState } from "react";
 
 const Card = ({ movie }) => {
-
+  const imageUrl = `https://image.tmdb.org/t/p/original${movie.poster_path}`;
 
   return (
-      <Stack direction={'column'} data-testid="movie-card">
-        <Box
-          mb={'20px'}
-          w={'full'}
-          h={['200px', '200px', '400px', '400px']}
-        >
-          <Img src="`url(https://image.tmdb.org/t/p/original${movie.poster_path})`" />
-        </Box>
-      </Stack>
+    <Stack direction={'column'} data-testid="movie-card">
+      <Box mb={'20px'} w={'full'} h={['200px', '200px', '400px', '400px']}>
+        <Img src={imageUrl} alt={movie.title} />
+      </Box>
+    </Stack>
   );
 }
 
 export default Card;
+
