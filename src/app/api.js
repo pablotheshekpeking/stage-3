662c2816d99ada3b2fbddd7c9837a8f34ff75eb6
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-21 13:07:29 +0300

    please work

--- src/app/api.js
+++ src/app/api.js
@@ -1,12 +1,12 @@
 // api.js
 import axios from 'axios';
 
-const apiKey = '6rgrQws5g38YYhiB5AXFhhSXKCaUM-N_sl-T7Mpt2_4'; // Replace with your Unsplash API key
+const apiKey = 'UmOk422CxVn4op9o3d-d20CBEwzA4WjDPipX6W-lo2A'; // Replace with your Unsplash API key
 
 const unsplashApi = axios.create({
   baseURL: 'https://api.unsplash.com',
   headers: {
-    Authorization: `Client-ID 6rgrQws5g38YYhiB5AXFhhSXKCaUM-N_sl-T7Mpt2_4`,
+    Authorization: `Client-ID ${apiKey}`,
   },
 });
 
