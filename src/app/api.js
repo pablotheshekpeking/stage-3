94a6dfb3d0af63994a4d84400023cd9538caa524
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-21 13:29:26 +0300

    im tired fa

--- src/app/api.js
+++ src/app/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const apiKey = 'e1C0H6D1KujiVMLXh5pgrASQYXtUfFDBmXsp0pNrV4SS38ud5z5buZfG'; // Replace with your Pexels API key
 
 const pexelsApi = axios.create({
-  baseURL: 'https://api.pexels.com/v1/search?query=nature&per_page=1',
+  baseURL: 'https://api.pexels.com',
   headers: {
     Authorization: apiKey,
   },
