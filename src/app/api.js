fcadb9750f782d4066e8c51d8c8d9fe1e2f32766
Michael Ibuaku <mujiangelo17@gmail.com>
2023-09-21 11:11:00 +0300

    again

--- src/app/api.js
+++ src/app/api.js
@@ -1,5 +1,5 @@
 // api.js
-
+import axios from 'axios';
 const apiKey = 'e1C0H6D1KujiVMLXh5pgrASQYXtUfFDBmXsp0pNrV4SS38ud5z5buZfG'; // Replace with your Pexels API key
 
 const tmdbApi = axios.create({
