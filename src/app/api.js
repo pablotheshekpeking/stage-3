f7f72435c1a146c6f9957ac752765b9f70f67319
Michael Ibuaku <86227443+pablotheshekpeking@users.noreply.github.com>
2023-09-22 14:46:25 +0300

    Update api.js

--- src/app/api.js
+++ src/app/api.js
@@ -2,6 +2,8 @@
 import axios from 'axios';
 
 const apiKey = 'e1C0H6D1KujiVMLXh5pgrASQYXtUfFDBmXsp0pNrV4SS38ud5z5buZfG'; // Replace with your Pexels API key
+const apiUrl = 'https://cors-anywhere.herokuapp.com/https://api.pexels.com/v1/curated?per_page=1';
+
 
 const pexelsApi = axios.create({
   baseURL: 'https://api.pexels.com/v1/curated?per_page=1',
